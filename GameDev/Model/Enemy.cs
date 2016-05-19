c936679ed483b7b5cfc0b5f4f37599a0dc65e8fa
josiahelmer <jelm9149@740s-pl-112.local>
2016-05-19 13:34:09 -0600

    bug fixes

--- GameDev/Model/Enemy.cs
+++ GameDev/Model/Enemy.cs
@@ -1,44 +1,100 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using GameDev.Model;
+using GameDev.View;
 
 
-namespace GameDev
+
+namespace GameDev.Model
 {
 	public class Enemy
 	{
-		private Animation enemyAnimation;
-		// Animation representing the enemy
-		public Animation EnemyAnimation 
-		{
-			get { return enemyAnimation; }
-			set { enemyAnimation = value; }
-		}
+			// Animation representing the enemy
+			private Animation enemyAnimation;
 
-		private Vector2 position;
-		// The positon of the enemy shi[ relative to the top left corner of thescreen
-		public Vector2 position 
-		{
-			get { return position; }
-			set { enemyAnimation = value; }
-		}
-		//The state of the Enemy Ship
-		public bool Active;
-		{
-			get { return active; }
-			set { active = value; }
-		}
+			// The position of the enemy ship relative to the top left corner of thescreen
+			public Vector2 Position;
 
-		private int health;
-		// The hit points of the enemy. if this goes to zero the enemy dies
-		public int Health;
+			// The state of the Enemy Ship
+			public bool Active;
 
-		//The amount of dameage the enemy inflicts on the player ship
-		public int Damage;
+			// The hit points of the enemy, if this goes to zero the enemy dies
+			public int Health;
 
-		public Enemy ()
-		{
+			// The amount of damage the enemy inflicts on the player ship
+			public int Damage;
+
+			// The amount of score the enemy will give to the player
+			public int Value;
+
+			// Get the width of the enemy ship
+			public int Width
+			{
+				get { return EnemyAnimation.FrameWidth; } 
+			}
+
+			// Get the height of the enemy ship
+			public int Height
+			{
+				get { return EnemyAnimation.FrameHeight; } 
+			}
+
+			// The speed at which the enemy moves
+			float enemyMoveSpeed;
+
+			public void Initialize(Animation animation,Vector2 position)
+			{
+				// Load the enemy ship texture
+				EnemyAnimation = animation;
+
+				// Set the position of the enemy
+				Position = position;
+
+				// We initialize the enemy to be active so it will be update in the game
+				Active = true;
+
+
+				// Set the health of the enemy
+				Health = 10;
+
+				// Set the amount of damage the enemy can do
+				Damage = 10;
+
+				// Set how fast the enemy moves
+				enemyMoveSpeed = 6f;
+
+
+				// Set the score value of the enemy
+				Value = 100;
+
+			}
+
+
+			public void Update(GameTime gameTime)
+			{ 
+				// The enemy always moves to the left so decrement it's xposition
+				Position.X -= enemyMoveSpeed;
+
+				// Update the position of the Animation
+				EnemyAnimation.Position = Position;
+
+				// Update Animation
+				EnemyAnimation.Update(gameTime);
+
+				// If the enemy is past the screen or its health reaches 0 then deactivateit
+				if (Position.X < -Width || Health <= 0)
+				{
+					// By setting the Active flag to false, the game will remove this objet fromthe
+					// active game list
+					Active = false;
+				}
+			}
+
+			public void Draw(SpriteBatch spriteBatch)
+			{
+				// Draw the animation
+				EnemyAnimation.Draw(spriteBatch);
+			}
 		}
 	}
-}
-
--- GameDev/Model/Player.cs
+++ GameDev/Model/Player.cs
@@ -1,44 +1,103 @@
 ﻿using System;
-using Microsoft.Xna.Framework
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using AfternoonGame.View;
+using GameDev.View;
 
 namespace GameDev.Model
 {
 	public class Player
 	{
+		#region Declaration Section
+		private Animation playerAnimation;
+		private Vector2 origin;
 		private int score;
 		private bool active;
 		private int health;
-		private Animation playerAnimation;
 
 		// Animation representing the player
-		public Animation PlayerAnimation
-		{
-			get { return PlayerAnimation; }
-			set { playerAnimation = value; }
-		}
-		//Animation representing the player
 		public Texture2D PlayerTexture;
 
-		//Postion of the Player relative to the upper left side of the screen
+		// Position of the Player relative to the upper left side of the screen
 		public Vector2 Position;
+		#endregion
 
-		//State of the player
+		public Animation PlayerAnimation
+		{
+			get{ return playerAnimation; }
+			set{ playerAnimation = value; }
+		}
+
+		#region Variable Properties (Getters/Setters)
+		//Properties for Variables
 		public bool Active
 		{
-			get { return active; }
-			set { active = value;
-
+			get{ return active; }
+			set{ active = value; }
 		}
+
+		public int Health 
+		{
+			get{ return health; }
+			set{ health = value; }
+		}
+
+		// Get the width of the player ship
+		public int Width
+		{
+			get { return playerAnimation.FrameWidth; }
+		}
+
+		// Get the height of the player ship
+		public int Height
+		{
+			get { return playerAnimation.FrameHeight; }
+		}
+
 		public int Score
 		{
-			get { return score; }
-			set { score = value; }
+			get{ return score; }
+			set{ score = value; }
+		}
+		#endregion
+
+		#region Initialize
+		public void Initialize(Animation animation, Vector2 position)
+		{
+			PlayerAnimation = animation;
+
+			// Set the starting position of the player around the middle of the screen and to the back
+			Position = position;
+
+			// Set the player to be active
+			active = true;
+
+			// Set the player health
+			health = 100;
+
+			origin.X = animation.FrameWidth / 2;
+			origin.Y = animation.frameHeight / 2;
+			// Set the player to be active
+			active = true;
+
+			// Set the player health
+			health = 100;
+
+			//Set the player score
+			score = 0;
+		}
+		#endregion
+
+
+		// Update the player animation
+		public void Update(GameTime gameTime)
+		{
+			PlayerAnimation.Position = Position;
+			PlayerAnimation.Update(gameTime);
 		}
 
-		public Player ()
+		public void Draw(SpriteBatch spriteBatch)
 		{
+			PlayerAnimation.Draw(spriteBatch);
 		}
 	}
 }
